03d95114d138d89ec611cb870d75e11ebd6488e6
Shankari02 <shankari.ak0208@gmail.com>
2024-09-30 01:14:26 +0530

    Added updated  mcpwm example code and include files

--- samples/mcpwm/src/main.c
+++ samples/mcpwm/src/main.c
@@ -13,15 +13,10 @@ static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 #define MOTOR_SPEED 60
 #define MOTOR_DELAY_MS 1000
 
-int main(void)
+void motor_thread(void)
 {   
-    if (!pwm_is_ready_dt(&pwm_led0)) {
-		printk("Error: PWM device %s is not ready\n",
-		       pwm_led0.dev->name);
-		return 0;
-	}
-    
-    struct device *motor1 = device_get_binding(DT_LABEL(DT_NODELABEL(pwm_led0)));
+       
+    struct device *motor1 = device_get_binding(DT_NODELABEL(pwm_led0));
     if (motor1 == NULL) {
         LOG_ERR("Motor device not found");
         return;
@@ -42,7 +37,6 @@ int main(void)
         LOG_INF("Stop");
         k_sleep(K_MSEC(MOTOR_DELAY_MS));
     }
-    return 0;
 }
 
-//K_THREAD_DEFINE(motor_thread_id, 1024, motor_thread, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(motor_thread_id, 1024, motor_thread, NULL, NULL, NULL, 7, 0, 0);
