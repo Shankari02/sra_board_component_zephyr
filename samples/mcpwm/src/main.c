8fd3a9a2b79d82c4aa197919076e41bd876a768e
Shankari02 <shankari.ak0208@gmail.com>
2024-11-19 23:50:35 +0530

    Minor edits in mcpwm src

--- samples/mcpwm/src/main.c
+++ samples/mcpwm/src/main.c
@@ -5,6 +5,8 @@
 // #include <zephyr/logging/log.h>
 
 static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+static const struct pwm_dt_spec pwm_led1 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led1));
+//static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 #if DT_NODE_HAS_STATUS(DT_ALIAS(pwm_led0), okay)
 #define PWM_DEV_NODE DT_ALIAS(pwm_led0)
@@ -12,6 +14,7 @@ static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 #error "Define a PWM device"
 #endif
 
+int period = PWM_SEC(1U) / 128U;
 int main(void)
 {
     if (!pwm_is_ready_dt(&pwm_led0))
@@ -20,18 +23,25 @@ int main(void)
                pwm_led0.dev->name);
         return 0;
     }
+    if (!pwm_is_ready_dt(&pwm_led1))
+    {
+        printk("Error: PWM device %s is not ready\n",
+               pwm_led1.dev->name);
+        return 0;
+    }
     motor_enable();
     printk("Motor enabled\n");
 
     while (1)
     {
         printk("Forward");
-        motor_forward();
+        motor1_set_speed(period);
+        motor2_set_speed(period);
         k_sleep(K_SECONDS(4U));
 
-        printk("Backward");
-        motor_backward();
-        k_sleep(K_SECONDS(4U));
+        // printk("Backward");
+        // motor_backward();
+        // k_sleep(K_SECONDS(4U));
 
         // motor_brake();
         // printk("Stop");
